1bc9ff359fe834b0b478df5218a35e4136d086ae
TonyFeng <sgtonyn@gmail.com>
2024-02-25 13:34:50 +0700

    deploy Update: deployment configuration and server port

--- shopdev-backend/server.js
+++ shopdev-backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server start with port ${PORT}`);
