a90b8106892884a701652d2cf5f90f2c63f9fc93
TonyFeng <sgtonyn@gmail.com>
2024-02-25 12:36:33 +0700

    Refactor: API endpoint in index.router.js

--- shopdev-backend/src/v1/routes/index.router.js
+++ shopdev-backend/src/v1/routes/index.router.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "server ok",
+  });
+});
 
 module.exports = router;
