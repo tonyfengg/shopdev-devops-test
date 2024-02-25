f8442482ddaf38ee4a73fd9b686bb9ec10cbaa42
TonyFeng <sgtonyn@gmail.com>
2024-02-25 11:43:22 +0700

    init: projects

---
+++ shopdev-backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server start with port ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => console.log(`exits server express`));
+});
